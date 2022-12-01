75a6cc6cf4c1fa6c3a5393d7bdee64532daf454f
omc-e <emsar.omic@outlook.com>
2022-12-01 23:37:06 +0100

    Cart Page Design

--- amazon_clone_app/lib/common/widgets/bottom_bar.dart
+++ amazon_clone_app/lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone_app/constants/global_variables.dart';
 import 'package:amazon_clone_app/features/account/screens/account_screen.dart';
+import 'package:amazon_clone_app/features/cart/screens/cart_screen.dart';
 import 'package:amazon_clone_app/features/home/screens/home_screen.dart';
 import 'package:amazon_clone_app/providers/user_provider.dart';
 import 'package:badges/badges.dart';
@@ -23,9 +24,7 @@ class _BottomBarState extends State<BottomBar> {
   List<Widget> pages = [
     const HomeScreen(),
     const AccountScreen(),
-    const Center(
-      child: Text('Cart Page'),
-    ),
+    const CartScreen(),
   ];
 
   void updatePage(int page) {
